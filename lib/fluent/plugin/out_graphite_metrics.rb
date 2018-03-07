23abeabffbe6fe14cdf310fa698cea784e9ec65d
Alex Yamauchi <oss@hotschedules.com>
2018-03-07 22:27:38 +0000

    INFRA-4104: I think this should work

--- lib/fluent/plugin/out_graphite_metrics.rb
+++ lib/fluent/plugin/out_graphite_metrics.rb
@@ -20,7 +20,6 @@ module Fluent
       super(conf) {
         @graphite_uri = conf.delete('graphite_uri')
         @prefix = conf.delete('prefix')
-        @instance_id = conf.delete('instance_id')
         @counter_maps = conf.delete('counter_maps')
         @counter_defaults = conf.delete('counter_defaults')
         @metric_maps = conf.delete('metric_maps')
@@ -28,7 +27,7 @@ module Fluent
       }
 
       @base_entry = {}
-      @base_entry['prefix'] = @prefix if @prefix
+      @base_entry['prefix'] = @prefix.nil? or @prefix.empty? ? '' : @prefix + '.'
 
     end
 
@@ -86,58 +85,43 @@ module Fluent
         end
       end
 
-      if data
-        stackdriver_to_graphite(data).each do |t,metrics|
-          post(metrics,t)
+      post(data) if data
+
+      end
+
+    end
+
+  end
+
+  def post(data)
+    # The module started life posting to the Stackdriver API.  Reformulate
+    # and push in Graphite API format instead of having to reformulate all
+    # of the parsing.
+
+    data.group_by{|e| e['collected_at']}.sort.each do |t,vals|
+      trial ||= 1
+      @client.metrics(Hash[vals.map{|e| e['name'] => e['value'] }], t)
+      rescue Errno::ETIMEDOUT
+        if trial <= @max_retries
+          log.warn "out_graphite_metrics: connection timeout to #{@graphite_uri}. Reconnecting... "
+          trial += 1
+          connect_client!
+          retry
+        else
+          log.error "out_graphite_metrics: ERROR: connection timeout to #{@graphite_uri}. Exceeded max_retries #{@max_retries}"
         end
-      end
-
+      rescue Errno::ECONNREFUSED
+        log.warn "out_graphite_metrics: connection refused by #{@graphite_uri}"
+      rescue SocketError => se
+        log.warn "out_graphite_metrics: socket error by #{@graphite_uri} :#{se}"
+      rescue StandardError => e
+        log.error "out_graphite_metrics: ERROR: #{e}"
     end
 
   end
 
-  def stackdriver_to_graphite(metrics)
-    # Convert the Stackdriver API format to Graphite format so that the
-    # datastructure and parser don't have to be completely refactored.
-
-    data = {}
-
-    metrics.each do |elem|
-      if not data.key?(elem['collected_at'])
-        data[elem['collected_at']] = {}
-      end
-      data[elem['collected_at']][elem['name']] = elem['value']
-    end
-
-    done
-
-    yield data
-
-  end
-
-  def post(metrics, time)
-    trial ||= 1
-    @client.metrics(metrics, time)
-  rescue Errno::ETIMEDOUT
-    # after long periods with nothing emitted, the connection will be closed and result in timeout
-    if trial <= @max_retries
-      log.warn "out_graphite_metrics: connection timeout to #{@host}:#{@port}. Reconnecting... "
-      trial += 1
-      connect_client!
-      retry
-    else
-      log.error "out_graphite_metrics: ERROR: connection timeout to #{@host}:#{@port}. Exceeded max_retries #{@max_retries}"
-    end
-  rescue Errno::ECONNREFUSED
-    log.warn "out_graphite_metrics: connection refused by #{@host}:#{@port}"
-  rescue SocketError => se
-    log.warn "out_graphite_metrics: socket error by #{@host}:#{@port} :#{se}"
-  rescue StandardError => e
-    log.error "out_graphite_metrics: ERROR: #{e}"
-  end
-  
   def connect_client!
-    @client = GraphiteAPI.new(graphite: "#{@host}:#{@port}")
+    @client = GraphiteAPI.new(graphite: @graphite_uri)
   end
 
 end
